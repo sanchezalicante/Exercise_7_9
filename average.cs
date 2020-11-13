ceb3cf5552b8b0c0d479365c62c7659a99a2552f
Jesus Sanchez <sanchezalicante@gmail.com>
2020-11-13 11:50:48 +0100

    Edit average.cs with clean code

--- average.cs
+++ average.cs
@@ -1,20 +1,35 @@
+/*Program to calculate the average of the numbers entered by the
+ *user until he types "end".
+ *Corrected by Jesus Sanchez.
+ */
 using System;
-public class Average{
-public static void Main(){
-double a=0, b, c=0, d;
-string s;
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();	// we read the mark as string to check the word "end"	 
-while(s!="end"){  // if the mark isn't an end then we pass it to double to work with it
-b=Convert.ToDouble(s);
-c++;   //counter of numbers
-a+=b;  // sum of all the notes
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();
-}
-if(c!=0){
-	d=a/c;  //calculate the average
-Console.WriteLine("The average is {0}.",d);
-}
-}
+
+public class Average
+{
+	public static void Main()
+	{
+		double userNumber, summedNumber = 0, counter = 0, average;
+		string input;
+		try
+		{
+			Console.Write("Insert a mark: ");
+			input = Console.ReadLine();
+			while((input != "end") && (input != "END") 
+				&& (input != "End"))
+			{  
+				userNumber = Convert.ToDouble(input);
+				counter ++;   
+				summedNumber += userNumber;  
+				
+				Console.Write("Insert another mark or \"END\": ");
+				input = Console.ReadLine();
+			}
+			average = summedNumber / counter;
+			Console.WriteLine("The average is {0}",average);
+		}
+		catch(FormatException)
+		{
+			Console.Write("Type a valid number or END");
+		}
+	}	
 }
